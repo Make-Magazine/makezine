8381ba5a67b3928bf17d22f07dce4946ab892bcf
Bill Olson <bolson@makermedia.com>
2015-03-12 15:10:56 -0700

    Radio Shack Sponsor Items

--- wp-content/themes/makeblog/page-weekend-projects-full.php
+++ wp-content/themes/makeblog/page-weekend-projects-full.php
@@ -9,26 +9,43 @@
  * 
  */
 get_header(); ?>
-		
+
+	<div class="projects-home">
+
+		<div class="container">
+
+			<div class="row">
+
+				<div class="span12">
+
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/projects-banner.png" alt="Weekend Projects" style="margin-top:-20px;margin-bottom:-20px;">
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
 
 	<div class="grey">
 
 		<div class="container">
-			
+
 			<div class="row">
-			
+
 				<div class="span12">
-					
+
 					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 						<?php the_content(); ?>
 					<?php endwhile; endif; ?>
-					
+
 				</div>
 
 			</div>
-		
+
 		</div>
 
-	</div>			
+	</div>
 
 <?php get_footer(); ?>
--- wp-content/themes/makeblog/page-weekend-projects-two-col.php
+++ wp-content/themes/makeblog/page-weekend-projects-two-col.php
@@ -9,28 +9,45 @@
  * 
  */
 get_header(); ?>
-		
-					
+
+	<div class="projects-home">
+
+		<div class="container">
+
+			<div class="row">
+
+				<div class="span12">
+
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/projects-banner.png" alt="Weekend Projects" style="margin-top:-20px;margin-bottom:-20px;">
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
 	<div class="grey">
 
 		<div class="container">
-			
+
 			<div class="row">
-			
+
 				<div class="span8">
-					
+
 					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 						<?php the_content(); ?>
 					<?php endwhile; endif; ?>
-					
+
 				</div>
 
 				<?php get_sidebar(); ?>
 
 			</div>
-		
+
 		</div>
 
-	</div>			
+	</div>
 
 <?php get_footer(); ?>
