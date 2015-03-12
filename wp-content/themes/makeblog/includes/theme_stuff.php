c5f8de79f588140cbabf1319afcf3e55c22aef05
Bill Olson <bolson@makermedia.com>
2015-03-12 13:36:03 -0700

    Removing quantcast script (unused)

--- wp-content/themes/makeblog/includes/theme_stuff.php
+++ wp-content/themes/makeblog/includes/theme_stuff.php
@@ -527,40 +527,6 @@ if ( ! function_exists( 'post_is_in_descendant_category' ) ) {
 }
 
 /**
- * Adds the Quantcast tag to the bottom of the page.
- * @return string Quantcast Javascript tracking code.
- */
-function make_quantcast_tag() { ?>
-		<!-- Quantcast Tag -->
-		<script type="text/javascript">
-			var _qevents = _qevents || [];
-
-			(function() {
-				var elem = document.createElement('script');
-				elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
-				elem.async = true;
-				elem.type = "text/javascript";
-				var scpt = document.getElementsByTagName('script')[0];
-				scpt.parentNode.insertBefore(elem, scpt);
-			})();
-
-			_qevents.push({
-				qacct:"p-c0y51yWFFvFCY"
-			});
-		</script>
-
-		<noscript>
-			<div style="display:none;">
-				<img src="//pixel.quantserve.com/pixel/p-c0y51yWFFvFCY.gif" border="0" height="1" width="1" alt="Quantcast"/>
-			</div>
-		</noscript>
-		<!-- End Quantcast tag -->
-<?php }
-
-add_action('wp_footer', 'make_quantcast_tag');
-
-
-/**
  * Add the Qualtrics popup to the Footer
  */
 function make_qualtrics_script() {
