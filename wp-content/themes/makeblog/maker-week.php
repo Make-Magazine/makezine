32669d933601bd7fde7b3613088d6685ee53dd59
maxreality <matt@makermedia.com>
2015-05-13 15:28:54 -0700

    MW thumbnails

--- wp-content/themes/makeblog/maker-week.php
+++ wp-content/themes/makeblog/maker-week.php
@@ -41,7 +41,8 @@
 				$output = '<div class="span8 mw-news">';
 				foreach ( $posts as $post ) {
 					$output .= '<div class="thumb slider">';
-					$output .= get_the_image( array( 'image_scan' => true, 'size' => 'maker-week-thumb', 'image_class' => 'hide-thumbnail pull-left', 'echo' => false ) );
+					$output .= get_the_post_thumbnail($post->ID);
+					
 					$output .= '<div class="mw-content">';
 					$output .= '<h4><a href="' . get_permalink() . '">' . make_get_short_title( 78 ) . '</a></h4>';
 					$output .= '<div class="clearfix"></div></div></div>';
