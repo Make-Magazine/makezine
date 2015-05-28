1f12afd2cd6f2f75079682cc91d5a59f6a66db3d
maxreality <matt@makermedia.com>
2015-05-28 13:43:06 -0600

    Testing featured images.

--- wp-content/themes/makeblog/home-header-nomal.php
+++ wp-content/themes/makeblog/home-header-nomal.php
@@ -36,11 +36,11 @@
 													if ( make_get_cap_option( 'main_id' ) ) {
 														echo wp_get_attachment_image( absint( make_get_cap_option( 'main_id' ) ), 'p1', 0, array( 'class' => $ribbon_class ) );
 													} else {
-														$main_url_id = make_get_cap_option( 'main_url' );
-														echo '<img src="'.get_resized_remote_image_url($main_url_id,301,400).'" height="400" width="301" alt="">';
+														echo '<img src="'.get_resized_remote_image_url($mainurl,301,400).'" height="400" width="301" alt=""';
 														if ( make_get_cap_option( 'ribbon_title_display' ) )
 															echo 'id="top-left" ';
 														echo '/>';
+
 													}
 												?>
 
