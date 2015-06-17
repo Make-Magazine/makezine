fbd545e00e1996083e7b41ccd53f9e4c07dac824
maxreality <matt@makermedia.com>
2015-06-17 17:13:26 -0600

    [mz-24] changes

--- wp-content/themes/makeblog/page-day-of-making-2.php
+++ wp-content/themes/makeblog/page-day-of-making-2.php
@@ -231,7 +231,7 @@ get_header(); ?>
 
 		<div class="row second-row">
 			
-			<a class="twitter-timeline" href="https://twitter.com/search?q=%23NationOfMakers" width="275" height="775" data-widget-id="476445295467704320">Tweets about "#NationOfMakers"</a>
+			<a class="twitter-timeline" href="https://twitter.com/search?q=%23NationOfMakers" width="275" height="770" data-widget-id="476445295467704320">Tweets about "#NationOfMakers"</a>
 			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 		
 		</div>
