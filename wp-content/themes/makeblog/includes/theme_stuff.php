3879c6dd2aad09c1e1f7845fb1715cbe92929c7d
David Beauchamp <dbeauchamp@makermedia.com>
2015-07-24 15:08:06 -0700

    Adding unique class names to the SumoMe buttons.

--- wp-content/themes/makeblog/includes/theme_stuff.php
+++ wp-content/themes/makeblog/includes/theme_stuff.php
@@ -1764,6 +1764,17 @@ function sumome_scroll_show_script() { ?>
 			        jQuery('.sumome-share-client-wrapper-left-page').fadeOut();
 			    }
 			});
+			jQuery(document).ready(function(){
+			    function addSumoMeClasses(){
+					jQuery("a[title='Twitter']").addClass("SumoMeTwitter")
+					jQuery("a[title='Facebook']").addClass("SumoMeFacebook")
+					jQuery("a[title='Google+']").addClass("SumoMeGplus")
+					jQuery("a[title='Reddit']").addClass("SumoMeReddit")
+					jQuery("a[title='LinkedIn']").addClass("SumoMeLinkedin")
+					//jQuery("a[title='More']").addClass("")
+			    };
+			    window.setTimeout( addSumoMeClasses, 3000 ); // 3 seconds
+			});
 		</script>
 	<?php
 }
