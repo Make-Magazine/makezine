94ac2c3c653244dcc97eabb7ee9dbca2c7360eca
Rich Haynie <rhaynie@slashdotmedia.com>
2015-10-21 16:07:07 -0600

    Promos.

--- wp-content/themes/makeblog/version-2/page-home-alpha.php
+++ wp-content/themes/makeblog/version-2/page-home-alpha.php
@@ -181,35 +181,24 @@ $menu_exists = wp_get_nav_menu_object( $menu_name );
 
 
 <!--  Berlin / School / World Maker Faire -->
-
       <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" id="event-faire">
         <a href="//makercamp.com">             
           <img src="<?php echo get_template_directory_uri().'/version-2/img/promo-makercamp-02.jpg' ?>" class="img-responsive center-block event" />
         </a>
       </div> 
-
-      <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" id="event-camp"> 
+      <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" id="event-camp">
         <a href="http://makerfaire.com/new-york-2015/slideshow/?utm_source=makezine.com&utm_medium=ads&utm_campaign=cross+site+promo&utm_creative=WMF+highlights">          
           <img src="<?php echo get_template_directory_uri().'/version-2/img/promo_world_maker5.jpg' ?>" class="img-responsive center-block event" />
         </a>
       </div> 
-  
-      <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" id="event-sponsored"> 
-        <a href="http://makerfaire.com/global/school/?utm_source=
-makezine.com&utm_medium=ads&utm_campaign=cross+site+promo&utm_creative=maker+faire+101">    
-          <img src="<?php echo get_template_directory_uri().'/version-2/img/150915-cross-site-promos-r5-08a-2.jpg' ?>" class="img-responsive center-block event" />
-        </a>
+      <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" id="event-sponsored">
+                <a href="//makezine.com/tag/halloween/">
+          <img src="<?php echo get_template_directory_uri().'/version-2/img/promos/promo_halloween10.jpg' ?>" class="img-responsive center-block event" />
+                </a>
       </div> 
-
-
-
-
-
-
-
     </div>  
   </div>
-  
+
   <!-- MAKER SHED PANEL -->
   <div class="container">
     <div class="row product-wrapper"> 
