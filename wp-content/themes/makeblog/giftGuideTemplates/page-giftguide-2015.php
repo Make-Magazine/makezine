2f336aaa4c6aec9ee491bd5236d84e34c021def3
David Beauchamp <dbeauchamp@makermedia.com>
2015-10-29 12:03:58 -0700

    First round of setting up the Gift Guide templates

---
+++ wp-content/themes/makeblog/giftGuideTemplates/page-giftguide-2015.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Template Name: Holiday Gift Guide 2015
+ *
+ * @package    makeblog
+ * @license    http://opensource.org/licenses/gpl-license.php  GNU Public License
+ * 
+ */
+get_header('version-2'); ?>
+  
+<div id="gg2015">
+
+  <div class="container gg2015-header">
+
+    <div class="row">
+
+      <div class="col-sm-5 gg2015-border-right">
+
+        <a href="/giftguide2015">
+          <h3>THE ULTIMATE MAKERS</h3>
+          <h3 class="gg2015-big-font">GIFT GUIDE</h3>
+        </a>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+      </div>
+
+      <div class="col-sm-7 gg2015-nav">
+
+        <div class="col-sm-4">
+          <a href="/giftguide2015/drones"><h2>DRONES</h2></a>
+          <a href="#"><h2>CRAFT</h2></a>
+          <a href="#"><h2>MAKE BELIEVE</h2></a>
+        </div>
+
+        <div class="col-sm-4">
+          <a href="#"><h2>ELECTRONICS</h2></a>
+          <a href="#"><h2>ROBOTS</h2></a>
+          <a href="#"><h2>TOYS</h2></a>
+        </div>
+
+        <div class="col-sm-4">
+          <a href="#"><h2>WEARABLES</h2></a>
+          <a href="#"><h2>AUDIO</h2></a>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <div class="container gg2015-hero">
+
+    <img class="visible-xs-block gg2015-mobile-hero img-responsive" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/img/home_background_mobile_2.png" alt="Makey snow globe animation" />
+    <div class="gg2015-snow"></div>
+
+    <div class="col-sm-6 hidden-xs">
+
+      <div class="text-center">
+
+        <h1>THE ULTIMATE MAKERS<span>GIFT GUIDE</span></h1>
+        <h2>THE BEST TOOLS AND TOYS FOR THE ONES YOU LOVE</h2>
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-xs-12">
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+
+<?php get_footer(); ?>
