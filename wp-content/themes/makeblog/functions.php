2f3fa30d9adc18f7dbd2dc10b965905b6bebba16
Sandrik85 <alexander@quorstudio.com>
2016-01-13 14:32:31 +0200

    add function the_titlesmall( = '',  = '',  = true,  = false) {      = get_the_title();
        if (  && is_numeric() ) {
             = substr( , 0,  );
        }
    
        if ( strlen()> 0 ) {
             = apply_filters('the_titlesmall',  .  . , , );
            if (  )
                echo ;
            else
                return ;
        }
    
    q
    
    qq
    q
    q
    q

--- wp-content/themes/makeblog/functions.php
+++ wp-content/themes/makeblog/functions.php
@@ -2220,3 +2220,19 @@ function catch_first_image_story_nav() {
     }
     return $first_img;
 }
+
+function the_titlesmall($before = '', $after = '', $echo = true, $length = false) {
+    $title = get_the_title();
+
+    if ( $length && is_numeric($length) ) {
+        $title = substr( $title, 0, $length );
+    }
+
+    if ( strlen($title)> 0 ) {
+        $title = apply_filters('the_titlesmall', $before . $title . $after, $before, $after);
+        if ( $echo )
+            echo $title;
+        else
+            return $title;
+    }
+}
