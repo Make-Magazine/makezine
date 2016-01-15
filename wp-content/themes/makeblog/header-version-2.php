3faa9ec971b05628cc75c30677df17e6c7e6cec4
Sandrik85 <alexander@quorstudio.com>
2016-01-15 20:44:01 +0200

    change header

--- wp-content/themes/makeblog/header-version-2.php
+++ wp-content/themes/makeblog/header-version-2.php
@@ -7,10 +7,11 @@
  *
  */
 
+?>
+<?php 
 global $make;
 global $wp_query;
 global $post;
-
 require_once 'version-2/includes/Mobile_Detect.php';
 $detect = new Mobile_Detect;
 $post_per_page = 15;
@@ -31,8 +32,6 @@ if( $detect->isTablet() ){
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-  <meta name="description" content="Version2">
-  <meta name="author" content="">
 
   <link rel="icon" href="<?php bloginfo('siteurl'); ?>/favicon.ico?v=2" />
   <link rel="shortcut icon" href="<?php bloginfo('siteurl'); ?>/favicon.ico?v=2" />
@@ -80,9 +79,6 @@ if( $detect->isTablet() ){
   <link rel="icon" sizes="32x32" href="<?php bloginfo('siteurl'); ?>/favicon-32x32.png?v=2" >
   <link rel="icon" sizes="96x96" href="<?php bloginfo('siteurl'); ?>/favicon-96x96.png?v=2" >
 
-
-
-
   <link href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700" rel="stylesheet" type="text/css">
 
   <title><?php echo make_generate_title_tag(); ?></title>
@@ -92,16 +88,16 @@ if( $detect->isTablet() ){
   <meta property="fb:admins" content="1612614584" />
 
   <!-- javascript -->
-  <script src="http://code.jquery.com/jquery-latest.min.js"></script>
-  <script src="<?php echo get_template_directory_uri().'/version-2/js/bootstrap.min.js' ?>"></script>
+  <script src="//code.jquery.com/jquery-latest.min.js"></script>
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
-  <script type="text/javascript" src="<?php echo get_template_directory_uri(). '/js/fancybox.js' ?>"></script>
+  <script src="<?php echo get_template_directory_uri().'/version-2/js/bootstrap.min.js' ?>"></script>
+
   <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
   <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
   <script src="<?php echo get_template_directory_uri().'/version-2/js/ie-emulation-modes-warning.js' ?>"></script>
   <script src="<?php echo get_template_directory_uri().'/version-2/js/header.js' ?>"></script>
   <script src="<?php echo get_template_directory_uri().'/version-2/js/single-story.js' ?>"></script>
-
+  <script type="text/javascript" src="<?php echo get_template_directory_uri(). '/js/fancybox.js' ?>"></script>
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -127,7 +123,7 @@ if( $detect->isTablet() ){
   <!-- End Chartbeat Code -->
 
   <?php wp_head(); ?>
-
+  
   <?php 
   // Set Ads.
   $make->ad_vars = new MakeAdVars;
@@ -148,12 +144,11 @@ if( $detect->isTablet() ){
   <?php print $make->ads->ad_1x1; ?>
 
 
-
   <script type="text/javascript">
     dataLayer = [];
   </script>
 </head>
-<body>
+<body <?php body_class(); ?>>
 <!-- Google Universal Analytics -->
 
 <!-- Time-tracking for Custom Dimensions -->
@@ -209,34 +204,42 @@ $primary_cat_dimension = $primarycat[0];
   </script>
 <?php endif; ?>
 
-<!-- BANNER -->
-<div class="header-wrapper">
-  <header class="hidden-xs page-banner">
-    <div class="row" id="top-header">
-      <div class="col-lg-3 col-md-3 col-sm-3 hidden-xs"></div>
-      <div class="col-lg-6 col-md-6 col-sm-6 hidden-xs text-center top-nav">
-        <h6><a href="http://www.makershed.com/?utm_source=makezine.com&utm_medium=brand+bar&utm_campaign=shop+best+sellers&utm_term=shop+best+sellers">Shop Best Sellers at Maker Shed &rarr; Kits, Books, More!</a></h6>
+<header class="header-wrapper">
+  <!--<img src="<?php echo get_template_directory_uri().'/version-2/img/2-layers@2x.png' ?>"> -->
+
+  <!-- TOP BRAND BAR -->
+  <div class="hidden-xs top-header-bar-brand">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6 col-sm-offset-3 text-center">
+          <p class="header-make-img">
+            <a href="//www.makershed.com/?utm_source=makezine.com&utm_medium=brand+bar&utm_campaign=shop+best+sellers&utm_term=shop+best+sellers" target="_blank">Shop Best Sellers at Maker Shed &rarr; Kits, Books, More!</a>
+          </p>
+        </div>
+        <div class="col-sm-3">
+          <p class="header-sub-link pull-right">
+            <a id="trigger-overlay" href="#">SUBSCRIBE </a>
+          </p>
+        </div>
       </div>
-      <div class="col-lg-1 col-md-3 col-sm-3 hidden-xs text-center subscribe">
-        <h6><a href="https://readerservices.makezine.com/mk/default.aspx?">Subscribe
-            <img src="<?php echo get_template_directory_uri().'/version-2/img/2-layers@2x.png' ?>"></a></h6>
-      </div>
-    </div> <!-- row -->
-  </header>
+    </div>
+  </div>
 
   <div class="container panel header <?php echo $device ?>">
+
     <!--nav class="navbar navbar-default"-->
     <nav class="navbar navbar-default">
       <div class="row">
 
         <!-- LOGO & TAG LINE -->
-        <a href="<?php echo home_url(); ?>" class="logo-a"><div class="col-md-2 col-sm-4 col-xs-5 logo-text">
-            <img src="<?php echo get_template_directory_uri().'/version-2/img/make_logo.png' ?>" class="mz-logo" />
-            <br>
+          <div class="col-md-2 col-sm-4 col-xs-5 logo-text">
+            <a href="<?php echo home_url(); ?>" class="logo-a">
+                <img src="<?php echo get_template_directory_uri().'/version-2/img/make_logo.png' ?>" class="mz-logo" />
+            </a>
             <h5 class="mz-tag">We are all Makers</h5>
-            <h6><a href="https://readerservices.makezine.com/mk/default.aspx?"><?php _e( 'Subscribe', 'makeblog' ) ?></a></h6>
-          </div></a>
-
+            <h6><a href="https://readerservices.makezine.com/mk/default.aspx?pc=MK&pk=M5BMKZ"><?php _e( 'Subscribe', 'makeblog' ) ?></a></h6>
+          </div>
+       
 
         <!-- MENUS -->
         <div class="navbar-header">
@@ -247,12 +250,22 @@ $primary_cat_dimension = $primarycat[0];
             <span class="icon-bar"></span>
           </button>
         </div>
+
+        <!-- Desktop and Mobile Menu -->
         <div class="col-md-7 col-sm-8 col-xs-12 menu-container">
+
+          <!-- Optional Above Nav Promo Message. Settings In Theme Customizer -->
+          <?php if( get_theme_mod( 'make_header_promo_enable' ) != '') { ?>
+            <h3 id="promo-text-above-nav" class="hidden-xs">
+              <a href="<?php echo get_theme_mod( 'make_header_promo_link', '' ); ?>"><?php echo get_theme_mod( 'make_header_promo_text', '' ); ?></a>
+            </h3>
+          <?php } // end if ?>
+
           <!-- Collapsible Menu -->
           <div id="makezine-navbar-collapse-1" class="navbar-collapse">
+
             <!-- Mobile search -->
             <div class="hidden-md mz-search search-bar-mobile">
-
               <form role="search" method="get" class="search-form" action="<?php echo home_url(); ?>">
                 <label>
                   <input type="search" class="search-field" placeholder="" value="" name="s" title="">
@@ -260,39 +273,38 @@ $primary_cat_dimension = $primarycat[0];
                 <input type="submit" class="search-submit" value="Search" />
               </form>
             </div>
+
             <?php wp_nav_menu('menu=Make main&menu_class=nav navbar-nav'); ?>
 
             <div class="mz-social mobile-social">
               <h5>Follow Us</h5>
               <a href="http://facebook.com/makemagazine">
-            <span class="fa-stack fa-mz">
-              <i class="fa fa-circle fa-stack-2x"></i>
-              <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-            </span>
+                <span class="fa-stack fa-mz">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                </span>
               </a>
 
               <a href="http://twitter.com/make">
-            <span class="fa-stack fa-mz">
-              <i class="fa fa-circle fa-stack-2x"></i>
-              <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
-            </span>
+                <span class="fa-stack fa-mz">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                </span>
               </a>
 
               <a href="http://pinterest.com/makemagazine/">
-             <span class="fa-stack fa-mz">
-              <i class="fa fa-circle fa-stack-2x"></i>
-              <i class="fa fa-pinterest-p fa-stack-1x fa-inverse"></i>
-            </span>
+                 <span class="fa-stack fa-mz">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-pinterest-p fa-stack-1x fa-inverse"></i>
+                </span>
               </a>
 
               <a href="https://instagram.com/makemagazine/">
-            <span class="fa-stack fa-mz">
-              <i class="fa fa-circle fa-stack-2x"></i>
-              <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
-            </span>
+                <span class="fa-stack fa-mz">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
+                </span>
               </a>
-
-
 
               <div class="mz-footer-subscribe">
                 <?php
@@ -306,11 +318,11 @@ $primary_cat_dimension = $primarycat[0];
                 <form class="sub-form" action="http://whatcounts.com/bin/listctrl" method="POST">
                   <input type="hidden" name="slid" value="6B5869DC547D3D46B52F3516A785F101" />
                   <input type="hidden" name="cmd" value="subscribe" />
-                  <input type="hidden" name="custom_source" value="footer" />
+                  <input type="hidden" name="custom_source" value="Mobile Header" />
                   <input type="hidden" name="custom_incentive" value="none" />
                   <input type="hidden" name="custom_url" value="<?php echo $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]; ?>" />
                   <input type="hidden" id="format_mime" name="format" value="mime" />
-                  <input type="hidden" name="goto" value="<?php  echo $isSecure. $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]; ?>?thankyou=true" />
+                  <input type="hidden" name="goto" value="<?php  echo $isSecure. $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]; ?>?thankyou=true&subscribed-to=make-newsletter" />
                   <input type="hidden" name="custom_host" value="<?php echo $_SERVER["HTTP_HOST"]; ?>" />
                   <input type="hidden" name="errors_to" value="" />
                   <div class="mz-form-horizontal">
@@ -318,19 +330,17 @@ $primary_cat_dimension = $primarycat[0];
                     <input value="GO" class="btn-cyan" type="submit">
                   </div>
                 </form>
-              </div>
+              </div><!-- End subscribe div -->
               <h6>Copyright © 2004-2015 Maker Media, Inc.</br>
                 All rights reserved</h6>
-            </div>
-
-          </div>
-
-        </div>
+            </div><!-- End mobile-social div -->
+          </div><!-- End #makezine-navbar-collapse-1 -->
+        </div><!-- End .menu-container -->
 
         <div class="get-dark"></div>
+
         <!-- SEARCH -->
         <div class="col-md-1 hidden-xs mz-search search-bar">
-
           <form role="search" method="get" class="search-form" action="<?php echo home_url(); ?>">
             <input type="submit" class="sendsubmit" value="" />
             <label>
@@ -339,53 +349,71 @@ $primary_cat_dimension = $primarycat[0];
             </label>
           </form>
         </div>
+
+        <!-- Sticky Navbar -->
         <div class="col-lg-1 col-md-3 col-sm-3 hidden-xs subscribe sticky-subscribe">
-          <h6><a href="https://readerservices.makezine.com/mk/default.aspx?"><h5>Subscribe</h5>
-              <img src="<?php echo get_template_directory_uri().'/version-2/img/2-layers@2x.png' ?>"></a></h6>
+          <h6>
+            <a id="trigger-overlay" href="#">
+              <h5>Subscribe</h5>
+              <img src="<?php echo get_template_directory_uri().'/version-2/img/2-layers@2x.png' ?>">
+            </a>
+          </h6>
         </div>
 
         <!-- SOCIAL MEDIA ICONS -->
         <div class="col-md-2  hidden-sm hidden-xs text-center desktop-social">
           <div class="mz-social">
             <a href="http://facebook.com/makemagazine">
-            <span class="fa-stack fa-mz">
-              <i class="fa fa-circle fa-stack-2x"></i>
-              <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-            </span>
+              <span class="fa-stack fa-mz">
+                <i class="fa fa-circle fa-stack-2x"></i>
+                <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+              </span>
             </a>
 
             <a href="http://twitter.com/make">
-            <span class="fa-stack fa-mz">
-              <i class="fa fa-circle fa-stack-2x"></i>
-              <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
-            </span>
+              <span class="fa-stack fa-mz">
+                <i class="fa fa-circle fa-stack-2x"></i>
+                <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+              </span>
             </a>
 
             <a href="http://pinterest.com/makemagazine/">
-             <span class="fa-stack fa-mz">
-              <i class="fa fa-circle fa-stack-2x"></i>
-              <i class="fa fa-pinterest-p fa-stack-1x fa-inverse"></i>
-            </span>
+               <span class="fa-stack fa-mz">
+                <i class="fa fa-circle fa-stack-2x"></i>
+                <i class="fa fa-pinterest-p fa-stack-1x fa-inverse"></i>
+              </span>
             </a>
 
             <a href="https://instagram.com/makemagazine/">
-            <span class="fa-stack fa-mz">
-              <i class="fa fa-circle fa-stack-2x"></i>
-              <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
-            </span>
+              <span class="fa-stack fa-mz">
+                <i class="fa fa-circle fa-stack-2x"></i>
+                <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
+              </span>
             </a>
           </div>
-        </div>
-      </div> <!-- row -->
+        </div><!-- End .desktop-social -->
+      </div><!-- row -->
     </nav>
-
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="<?php echo get_template_directory_uri().'/version-2/js/ie10-viewport-bug-workaround.js' ?>"></script>
-  </div>
-  <div class="second-nav"></div>
-</div>
-<div class="dynamic-header-posts">
+
+  </div><!-- container panel header -->
+
+  <!-- Optional Under Nav Promo Blue Bar and Message. Settings In Theme Customizer -->
+  <?php if( get_theme_mod( 'make_header_bluebar_enable' ) != '') { ?>
+    <div class="second-nav promo-text-under-nav">
+      <div class="container hidden-xs">
+        <h3>
+          <a href="<?php echo get_theme_mod( 'make_header_bluebar_link', '' ); ?>"><?php echo get_theme_mod( 'make_header_bluebar_text', '' ); ?></a>
+        </h3>
+      </div>
+    </div>
+  <?php } // end if ?>
+
+</header><!-- header-wrapper -->
+
+<div class="dynamic-header-posts <?php if( get_theme_mod( 'make_header_bluebar_enable' ) != '') { ?>dynamic-header-posts-margin<?php } ?>">
   <div class="dynamic-header-container container">
     <div class="menu-container row">
       <div class="menu-sub-menu"></div>
@@ -440,7 +468,7 @@ $primary_cat_dimension = $primarycat[0];
               if(strlen($url) == 0){
                 $photon = catch_first_image_nav();
                 $photon = jetpack_photon_url( $photon, $args );
-              }?>
+              } ?>
               <img src="<?php echo $photon; ?>" alt="thumbnail">
             </a>
             <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
@@ -503,3 +531,4 @@ $primary_cat_dimension = $primarycat[0];
     </div>
   </div>
 </div>
+<div class="close-dynamic-content"></div>
