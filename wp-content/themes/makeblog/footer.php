f6ce3d8b8028c003754f885931608efc9576c389
David Beauchamp <dbeauchamp@makermedia.com>
2016-01-21 16:37:50 -0800

    Fixing issues Quora created in the footer.

--- wp-content/themes/makeblog/footer.php
+++ wp-content/themes/makeblog/footer.php
@@ -51,44 +51,48 @@
 
 			<div class="col-sm-12 col-sm-6 col-md-3 social-foot-col">
 				<h4 class="stay-connected">Follow Us</h4>
-				<div class="mz-footer-social">
-					<div class="col-xs-3">
-						<a href="http://facebook.com/makemagazine">
-								<span class="fa-stack fa-mz">
-									<i class="fa fa-circle fa-stack-2x"></i>
-									<i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-								</span>
-						</a>
-					</div>
+        <div class="social-network-container">
+          <ul class="social-network social-circle">
+            <li><a href="//facebook.com/makemagazine" class="icoFacebook" title="Facebook" target="_blank"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="//twitter.com/make" class="icoTwitter" title="Twitter" target="_blank"><i class="fa fa-twitter" target="_blank"></i></a></li>
+            <li><a href="//pinterest.com/makemagazine" class="icoPinterest" title="Pinterest" target="_blank"><i class="fa fa-pinterest-p" target="_blank"></i></a></li>
+            <li><a href="//instagram.com/makemagazine" class="icoInstagram" title="Instagram" target="_blank"><i class="fa fa-instagram" target="_blank"></i></a></li>
+          </ul>    
+        </div>
+        <div class="clearfix"></div>
+        
+        <div class="fancybox-feedback" style="display:none;">
+          <div class="fancybox-feedback-inner">
+            <div class="fancybox-feedback-inner1">
+              <h3>Makers, we want to hear from you!</h3>
+              <p>Send us feedback on our site design, bugs, story ideas, maker community events and any other share-worthy thoughts.</p>
+              <form id="form13" name="form13" accept-charset="UTF-8" enctype="multipart/form-data" method="post" novalidate action="//makemagazine.wufoo.com/forms/zzv65kl0b09v1f/#public">
+                <div class="form-group">
+                  <label for="exampleInputName1">Name</label>
+                  <input type="text" class="form-control" id="Field1" name="Field1" placeholder="Name">
+                  <p class="help-block">*Not Required</p>
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Email Address For Follow Up</label>
+                  <input type="email" class="form-control" id="Field2" name="Field2" placeholder="Email">
+                  <p class="help-block">*Not Required</p>
+                </div>
+                <div class="form-group">
+                  <label>Write Your Feedback Here</label>
+                  <textarea class="form-control" id="Field3" name="Field3" spellcheck="true" rows="3" required></textarea>
+                </div>
+                <input id="saveForm" name="saveForm" class="footer-feedback-submit btn-cyan" type="submit" value="Submit" />
+                <input type="hidden" id="idstamp" name="idstamp" value="WH028EiOADf/hZl3yIPszD+rnU6UVQ2++DXf7i7lt38=" />
+              </form>
+            </div>
+            <div class="fancybox-feedback-inner2" style="display:none;">
+              <h3>Thank you for the feedback!</h3>
+              <p>We can't guarantee a response to each submission, but we promise to think about every one.</p>
+            </div>
+          </div>
+        </div>
+        <div class="clearfix"></div>
 
-					<div class="col-xs-3">
-						<a href="http://twitter.com/make">
-								<span class="fa-stack fa-mz">
-									<i class="fa fa-circle fa-stack-2x"></i>
-									<i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
-								</span>
-						</a>
-					</div>
-
-					<div class="col-xs-3">
-						<a href="http://pinterest.com/makemagazine/">
-								<span class="fa-stack fa-mz">
-									<i class="fa fa-circle fa-stack-2x"></i>
-									<i class="fa fa-pinterest-p fa-stack-1x fa-inverse"></i>
-								</span>
-						</a>
-					</div>
-
-					<div class="col-xs-3">
-						<a href="https://instagram.com/makemagazine/">
-								<span class="fa-stack fa-mz">
-									<i class="fa fa-circle fa-stack-2x"></i>
-									<i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
-								</span>
-						</a>
-					</div>
-				</div>
-				<div class="clearfix"></div>
 				<div class="mz-footer-subscribe">
 					<?php
 					$isSecure = "http://";
@@ -120,45 +124,15 @@
 		<div class="row social-foot-mobile visible-xs-block">
 			<div class="col-xs-12 social-foot-col">
 				<h4 class="stay-connected">Follow Us</h4>
-				<div class="mz-footer-social">
-					<div class="col-xs-3">
-						<a href="http://facebook.com/makemagazine">
-								<span class="fa-stack fa-mz">
-									<i class="fa fa-circle fa-stack-2x"></i>
-									<i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-								</span>
-						</a>
-					</div>
-
-					<div class="col-xs-3">
-						<a href="http://twitter.com/make">
-								<span class="fa-stack fa-mz">
-									<i class="fa fa-circle fa-stack-2x"></i>
-									<i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
-								</span>
-						</a>
-					</div>
-
-					<div class="col-xs-3">
-						<a href="http://pinterest.com/makemagazine/">
-								<span class="fa-stack fa-mz">
-									<i class="fa fa-circle fa-stack-2x"></i>
-									<i class="fa fa-pinterest-p fa-stack-1x fa-inverse"></i>
-								</span>
-						</a>
-					</div>
-
-					<div class="col-xs-3">
-						<a href="https://instagram.com/makemagazine/">
-								<span class="fa-stack fa-mz">
-									<i class="fa fa-circle fa-stack-2x"></i>
-									<i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
-								</span>
-						</a>
-					</div>
-				</div>
-
-				<div class="clearfix"></div>
+        <div class="social-network-container">
+          <ul class="social-network social-circle">
+            <li><a href="//facebook.com/makemagazine" class="icoFacebook" title="Facebook" target="_blank"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="//twitter.com/make" class="icoTwitter" title="Twitter" target="_blank"><i class="fa fa-twitter" target="_blank"></i></a></li>
+            <li><a href="//pinterest.com/makemagazine" class="icoPinterest" title="Pinterest" target="_blank"><i class="fa fa-pinterest-p" target="_blank"></i></a></li>
+            <li><a href="//instagram.com/makemagazine" class="icoInstagram" title="Instagram" target="_blank"><i class="fa fa-instagram" target="_blank"></i></a></li>
+          </ul>    
+        </div>
+        <div class="clearfix"></div>
 
 				<div class="mz-footer-subscribe">
 					<?php
@@ -180,7 +154,7 @@
 						<input type="hidden" name="custom_host" value="<?php echo $_SERVER["HTTP_HOST"]; ?>" />
 						<input type="hidden" name="errors_to" value="" />
 						<div class="mz-form-horizontal">
-							<input name="email" placeholder="Enter your Email" required="required" type="text">
+							<input name="email" placeholder="Enter your Email" required type="text">
 							<input value="GO" class="btn-cyan" type="submit">
 						</div>
 					</form>
@@ -191,11 +165,11 @@
 			<div class="col-xs-12 panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 				<div class="panel panel-default">
 					<div class="panel-heading" role="tab" id="heading1">
-						<h4 class="panel-title">
-							<a data-toggle="collapse" data-parent="#accordion" href="#collapse1" aria-expanded="false" aria-controls="collapse1">Make:</a>
-						</h4>
+						<a data-toggle="collapse" data-parent="#accordion" href="#collapse1" aria-expanded="false" aria-controls="collapse1">
+							<h4 class="panel-title">Make:</h4>
+						</a>
 					</div>
-					<div id="collapse1" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading1">
+					<div id="collapse1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading1">
 						<div class="panel-body">
 							<ul class="nav nav-pills nav-stacked">
 								<li><a href="/projects/">Make: Projects</a></li>
@@ -209,11 +183,11 @@
 				</div>
 				<div class="panel panel-default">
 					<div class="panel-heading" role="tab" id="heading2">
-						<h4 class="panel-title">
-							<a data-toggle="collapse" data-parent="#accordion" href="#collapse2" aria-expanded="false" aria-controls="collapse2">Explore Making</a>
-						</h4>
+						<a data-toggle="collapse" data-parent="#accordion" href="#collapse2" aria-expanded="false" aria-controls="collapse2">
+							<h4 class="panel-title">Explore Making</h4>
+						</a>
 					</div>
-					<div id="collapse2" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading2">
+					<div id="collapse2" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading2">
 						<div class="panel-body">
 							<ul class="nav nav-pills nav-stacked">
 								<li><a href="//makerfaire.com/?utm_source=makezine.com&utm_medium=footer&utm_term=makerfaire" target="_blank">Maker Faire</a></li>
@@ -226,11 +200,11 @@
 				</div>
 				<div class="panel panel-default">
 					<div class="panel-heading" role="tab" id="heading3">
-						<h4 class="panel-title">
-							<a data-toggle="collapse" data-parent="#accordion" href="#collapse3" aria-expanded="false" aria-controls="collapse3">Our Company</a>
-						</h4>
+						<a data-toggle="collapse" data-parent="#accordion" href="#collapse3" aria-expanded="false" aria-controls="collapse3">
+							<h4 class="panel-title">Our Company</h4>
+						</a>
 					</div>
-					<div id="collapse3" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading3">
+					<div id="collapse3" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading3">
 						<div class="panel-body">
 							<ul class="nav nav-pills nav-stacked">
 								<li><a href="//makermedia.com" target="_blank">About Us</a></li>
@@ -244,6 +218,7 @@
 				</div>
 			</div>
 		</div><!-- End social-foot-mobile -->
+		<button class="footer-feedback-btn btn-cyan pull-center">Send Us Feedback</button>
 	</div><!-- END container -->
 	<?php echo make_copyright_footer(); ?>
 </section><!-- END new-footer -->
@@ -279,6 +254,37 @@
 
 <!-- Subscribe return path overlay -->
 <?php echo subscribe_return_path_overlay(); ?>
+
+<!-- Feedback form submit event handler -->
+<script>
+  $(document).on('submit', '#form13', function (e) {
+    event.preventDefault();
+    $.ajax({
+      url:'//makemagazine.wufoo.com/forms/zzv65kl0b09v1f/#public',
+      type:'POST',
+      data:$(this).serialize()
+    });
+    $('.fancybox-feedback-inner1').hide();
+    $('.fancybox-feedback-inner2').show();
+  });
+</script>
+
+<!-- Feedback form modal -->
+<script>
+  $(".footer-feedback-btn").click(function() {
+    $(".fancybox-feedback").fancybox({
+      autoSize : false,
+      width  : 560,
+      autoHeight : true,
+      padding : 0,
+      openEffect : 'elastic',
+      afterLoad   : function() {
+        this.content = this.content.html();
+      }
+    });
+    $(".fancybox-feedback").trigger('click');
+  });
+</script>
 
 <!-- AddRoll Retargeting Pixel -->
 <script type="text/javascript">
