dc3755c44b268f454fade673a713363033277067
Wesley Wilson <wes@wesww.com>
2016-02-23 17:58:20 -0800

    adding a dev mode to the less grunt task, for source comments

--- wp-content/themes/makeblog/Gruntfile.js
+++ wp-content/themes/makeblog/Gruntfile.js
@@ -1,50 +1,69 @@
-module.exports = function( grunt ) {
+module.exports = function(grunt) {
 
-	// All configurations go here
-	grunt.initConfig({
+  // All configurations go here
+  grunt.initConfig({
 
-		// Reads the package.json file
-		pkg: grunt.file.readJSON( 'package.json' ),
+    // Reads the package.json file
+    pkg: grunt.file.readJSON('package.json'),
 
-		// Each Grunt plugins configurations will go here
-		less: {
-			development: {
-		    options: {
-		      compress: true
-		    },
-				files: {
-					'css/style.css': 'less/style.less',
-					'css/print.css': 'less/make/print.less',
-					'css/takeover.css': 'less/make/takeover.less',
-					'css/day-of-making.css': 'less/day-of-making.less',
-					'version-2/css/style.css': 'version-2/less/style.less',
-					'reviews/css/master.css': 'reviews/less/master.less'
-				}
-			}
-		},
-		watch: {
-			css: {
-				files: ['less/**/*.less', 'version-2/less/**/*.less'],
-				tasks: ['less']
-			},
-			reload: {
-				files: ['less/**/*.less'],
-				tasks: ['less'],
-				options: {
-					livereload: true
-				}
-			}
-		}
-	});
+    // Each Grunt plugins configurations will go here
+    less: {
+      prod: {
+        options: {
+          compress: true
+        },
+        files: {
+          'css/style.css': 'less/style.less',
+          'css/print.css': 'less/make/print.less',
+          'css/takeover.css': 'less/make/takeover.less',
+          'css/day-of-making.css': 'less/day-of-making.less',
+          'version-2/css/style.css': 'version-2/less/style.less',
+          'reviews/css/master.css': 'reviews/less/master.less'
+        }
+      },
+      dev: {
+        options: {
+          compress: false,
+          dumpLineNumbers: 'comments'
+        },
+        files: {
+          'css/style.css': 'less/style.less',
+          'css/print.css': 'less/make/print.less',
+          'css/takeover.css': 'less/make/takeover.less',
+          'css/day-of-making.css': 'less/day-of-making.less',
+          'version-2/css/style.css': 'version-2/less/style.less',
+          'reviews/css/master.css': 'reviews/less/master.less'
+        }
+      }
+    },
+    watch: {
+      prod: {
+        files: ['less/**/*.less', 'version-2/less/**/*.less'],
+        tasks: ['less:prod']
+      },
+      dev: {
+        files: ['less/**/*.less', 'version-2/less/**/*.less'],
+        tasks: ['less:dev']
+      },
+      reload: {
+        files: ['less/**/*.less'],
+        tasks: ['less'],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
 
-	// Simplify the repetivite work of listing each plugin in grunt.loadNomTasks(), just get the list from package.json and load them...
-	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  // Simplify the repetivite work of listing each plugin in grunt.loadNomTasks(), just get the list from package.json and load them...
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-	// Register the tasks with Grunt
-	// To only watch for less changes and process without browser reload type in "grunt"
-	grunt.registerTask( 'default', ['less', 'watch:css'] );
-
-	// To watch for less changes and process them with livereload type in "grunt reload"
-	grunt.registerTask( 'reload', ['less', 'watch:reload'] );
+  // Register the tasks with Grunt
+  // To only watch for less changes and process without browser reload type in "grunt"
+  grunt.registerTask('default', ['less:prod', 'watch:prod']);
+  // Dev mode build task
+  grunt.registerTask('dev', ['less:dev', 'watch:dev']);
+  // To watch for less changes and process them with livereload type in "grunt reload"
+  grunt.registerTask('reload', ['less', 'watch:reload']);
 
 };
