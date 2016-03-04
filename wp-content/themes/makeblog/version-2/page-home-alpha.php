08099e209f270af37ec3436deb8f7be994291717
Bill Olson <bolson@makermedia.com>
2016-03-03 16:59:52 -0800

    MFBA Ticket promo for home page

--- wp-content/themes/makeblog/version-2/page-home-alpha.php
+++ wp-content/themes/makeblog/version-2/page-home-alpha.php
@@ -203,8 +203,8 @@ $menu_exists = wp_get_nav_menu_object( $menu_name );
         </a>
       </div> 
       <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" id="event-camp">
-        <a href="/comparison/3dprinters/how-we-test/shootout/">          
-          <img src="<?php echo get_template_directory_uri().'/version-2/img/promos/promo-3d-guide16.jpg' ?>" class="img-responsive center-block event" alt="3D Printer Guide Promo" />
+        <a href="//makerfaire.com/bay-area" target="_blank">          
+          <img src="<?php echo get_template_directory_uri().'/version-2/img/promos/promo-mfba-01.jpg' ?>" class="img-responsive center-block event" alt="Maker Faire Bay Area Ticket Promo" />
         </a>
       </div> 
     </div>  
