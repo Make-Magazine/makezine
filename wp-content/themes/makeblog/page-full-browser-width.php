686d06c8ad733b3b304924ed7e8873e9f43a32b9
David Beauchamp <dbeauchamp@makermedia.com>
2016-03-31 18:47:48 -0700

    New page template for the newsletter landing page.

--- wp-content/themes/makeblog/page-full-browser-width.php
+++ wp-content/themes/makeblog/page-full-browser-width.php
@@ -4,7 +4,6 @@
  *
  * @package    makeblog
  * @license    http://opensource.org/licenses/gpl-license.php  GNU Public License
- * @author     Cole Geissinger <cgeissinger@makermedia.com>
  * 
  */
 get_header('version-2'); ?>
