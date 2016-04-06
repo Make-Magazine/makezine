0ba5dd8db83f6c984757ea23b6767181c650b78b
David Beauchamp <dbeauchamp@makermedia.com>
2016-04-06 13:39:58 -0700

    Converting templates to BS 3. Also a ton of random style fixes.

--- wp-content/themes/makeblog/sidebar.php
+++ wp-content/themes/makeblog/sidebar.php
@@ -11,35 +11,34 @@
  */
 ?>
 
-					<div class="span4 sidebar">
+<div class="sidebar">
 
-						<div class="sidebar-ad">
-							<p id="ads-title">Advertisement</p>
-							<?php global $make; print $make->ads->ad_300x250_atf; ?>
-						</div>
+	<div class="sidebar-ad">
+		<p id="ads-title">Advertisement</p>
+		<?php global $make; print $make->ads->ad_300x250_atf; ?>
+	</div>
 
-						<?php dynamic_sidebar( 'sidebar_top' ); ?>
+	<?php dynamic_sidebar( 'sidebar_top' ); ?>
 
-						<div class="maker-camp-promo">
-							<?php
+	<div class="maker-camp-promo">
+		<?php
 
-								if( is_single( array( '53649' ))) {
-									echo '<a href="http://www.makershed.com/products/el-wire-starter-packs-10ft?utm_source=makezine.com&utm_medium=ads&utm_campaign=maker-camp&utm_keyword=El_Wire_Starter_Packs_10ft" target="_blank"><img src="https://makezineblog.files.wordpress.com/2014/07/elwire.gif" alt="EL-Wire Starter Pack from Maker Shed" /></a>';
-								}
-							 ?>
-						</div>
+			if( is_single( array( '53649' ))) {
+				echo '<a href="http://www.makershed.com/products/el-wire-starter-packs-10ft?utm_source=makezine.com&utm_medium=ads&utm_campaign=maker-camp&utm_keyword=El_Wire_Starter_Packs_10ft" target="_blank"><img src="https://makezineblog.files.wordpress.com/2014/07/elwire.gif" alt="EL-Wire Starter Pack from Maker Shed" /></a>';
+			}
+		 ?>
+	</div>
 
-						<div class="new-dotw widget">
-							<p id="ads-title">Advertisement</p>
-							<?php global $make; print $make->ads->ad_300x250_shed; ?>
-						</div>
-						
-						<div class="sidebar-ad">
-							<p id="ads-title">Advertisement</p>
-							<?php global $make; print $make->ads->ad_300x250_house; ?>
-						</div>
+	<div class="new-dotw widget">
+		<p id="ads-title">Advertisement</p>
+		<?php global $make; print $make->ads->ad_300x250_shed; ?>
+	</div>
+	
+	<div class="sidebar-ad">
+		<p id="ads-title">Advertisement</p>
+		<?php global $make; print $make->ads->ad_300x250_house; ?>
+	</div>
 
-						<?php dynamic_sidebar( 'sidebar_bottom' ); ?>
+	<?php dynamic_sidebar( 'sidebar_bottom' ); ?>
 
-
-				</div>
+</div>
