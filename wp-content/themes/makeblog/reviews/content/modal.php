c9a0bd2707e541e610a7858978110cdf8c7893d7
Bill Olson <bolson@makermedia.com>
2016-04-29 11:47:43 -0700

    Drones guide modal

--- wp-content/themes/makeblog/reviews/content/modal.php
+++ wp-content/themes/makeblog/reviews/content/modal.php
@@ -98,3 +98,51 @@
 	</div>
 </div>
 <!-- END BOARDS GUIDE NEWSLETTER MODAL -->
+
+<!-- DRONES GUIDE NEWSLETTER MODAL -->
+<script>
+	$(".fancyboxDrones").fancybox({
+		autoSize : false,
+		width  : 630,
+		autoHeight : true,
+		padding : 0,
+		afterLoad   : function() {
+			this.content = this.content.html();
+		}
+	});
+	$(document).ready(function () {
+		$( "#modal-capture-btn.class-drones" ).click(function() {
+			$(".fancyboxDrones").trigger('click');
+		});
+	});
+</script>
+<div class="fancyboxDrones" style="display:none;">
+	<div class="nl-modal-no-bg">
+		<div class="col-sm-8 col-xs-12 nl-modal">
+			<h3 class="text-center">Yes, I would like my free PDF copy of the Drone Flyer's Guide.</h3>
+			<?php $isSecure = "http://";
+			if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+				$isSecure = "https://";
+			} ?>
+			<form class="whatcounts-signup-footer" action="http://secure.whatcounts.com/bin/listctrl" method="POST">
+				<input type="hidden" name="slid" value="6B5869DC547D3D46B52F3516A785F101" />
+				<input type="hidden" name="cmd" value="subscribe" />
+				<input type="hidden" name="custom_source" value="modal-dronesPDF" />
+				<input type="hidden" name="custom_incentive" value="dronesPDF" />
+				<input type="hidden" name="custom_url" value="<?php echo $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]; ?>" />
+				<input type="hidden" id="format_mime" name="format" value="mime" />
+				<input type="hidden" name="goto" value="<?php  echo $isSecure. $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]; ?>?thankyou=true&subscribed-to=free-pdf" />
+				<input type="hidden" name="errors_to" value="" />
+				<div class="mz-form-horizontal">
+					<input name="email" placeholder="Enter your Email" required type="email"><br>
+					<input value="GO" class="btn-cyan" type="submit">
+				</div>
+			</form>
+			<p class=""><small>This is offered with a subscription to the Make: weekly newsletter; you can unsubscribe at any time.</small></p>
+		</div>
+		<div class="col-sm-4 hidden-xs nl-modal">
+			<img class="img-responsive" src="//makezine.com/wp-content/uploads/2016/04/Make_Drone-Flyers-Guide-Cover-2.jpg" alt="Make: Drones Flyer's Guide" />
+		</div>
+	</div>
+</div>
+<!-- END DRONES GUIDE NEWSLETTER MODAL -->
