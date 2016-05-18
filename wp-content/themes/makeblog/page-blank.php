06aafc838879c5b8aed8586ed179c0acb4c8ec4d
Rich Haynie <rhaynie@slashdotmedia.com>
2016-05-18 13:34:08 -0700

    GE Fixes.

---
+++ wp-content/themes/makeblog/page-blank.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: Blank Page
+ *
+ * @package    makeblog
+ * @license    http://opensource.org/licenses/gpl-license.php  GNU Public License
+ * @author     Rich Haynie
+ * 
+ */
+get_header('version-2');
+?>
+
+<div style="padding:0px" class="single">
+
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <?php the_content(); ?>
+  <?php endwhile;
+else: ?>
+
+    <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+
+<?php endif; ?>
+</div>
+
+
+
+<?php get_footer(); ?>
