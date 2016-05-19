ac0c8aeee84a46f0506f116a7aeac1dbe2aa25c4
Rich Haynie <rhaynie@slashdotmedia.com>
2016-05-19 15:37:36 -0700

    Fix for ge challenge.

--- wp-content/themes/makeblog/page-ge-light-life.php
+++ wp-content/themes/makeblog/page-ge-light-life.php
@@ -12,7 +12,7 @@ get_header('version-2');
  wp_register_style('et-googleFonts', 'http://fonts.googleapis.com/css?family=Roboto:300');
   wp_register_style('et-googleFonts', 'http://fonts.googleapis.com/css?family=Roboto+Slab:700');
   wp_enqueue_style('et-googleFonts');
-  wp_enqueue_style('ge-light-life', get_stylesheet_directory_uri() . '/css/ge-light-for-life.css',array(), '2.7.2' );
+  wp_enqueue_style('ge-light-life', get_stylesheet_directory_uri() . '/css/ge-light-for-life.css',array(), '2.7.3' );
 
 
 ?>
