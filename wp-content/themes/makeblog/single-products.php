b215dd54d9897ba3d519175f0093357cb3e67942
David Beauchamp <dbeauchamp@makermedia.com>
2016-06-15 11:47:56 -0700

    Deleting some old and unused templates

--- wp-content/themes/makeblog/single-products.php
+++ wp-content/themes/makeblog/single-products.php
@@ -6,31 +6,10 @@
 get_header('version-2'); ?>
 
 	
-	
 	<?php get_template_part( 'reviews/content/header/products' ); ?>
 	
 	
 	<?php get_template_part( 'reviews/content/single/products' ); ?>
 
 
-
 <?php get_footer(); ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
