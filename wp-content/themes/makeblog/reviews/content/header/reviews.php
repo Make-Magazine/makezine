c3108f8cd4a87769422c8d9fae005bfeac55b1fe
David Beauchamp <dbeauchamp@makermedia.com>
2016-09-15 11:54:44 -0700

    Fixing the reviews nav

--- wp-content/themes/makeblog/reviews/content/header/reviews.php
+++ wp-content/themes/makeblog/reviews/content/header/reviews.php
@@ -16,6 +16,7 @@ if ( is_singular( \Reviews\Architecture\Post_Types\Products::NAME ) ) {
 
 $modal_image    = get_field( 'magazine_thumbnail', $id );
 $modal_text     = get_field( 'magazine_label', $id );
+$slug  = \Reviews\Architecture\Post_Types\Reviews::get_product_category_slug( $id );
 
 ?>
 <div class="row">
@@ -38,37 +39,41 @@ $modal_text     = get_field( 'magazine_label', $id );
 			<?php endif; ?>
 
 			<div class="review-nav-choosing" <?php if ( \Reviews\Architecture\Post_Types\Reviews::is_how_we_test() ) { ?> class="active"  <?php } ?> >
-				<a href="<?php echo \Reviews\Architecture\Post_Types\Reviews::get_how_we_test_link( $id ); ?>">
-					<?php
-					$slug  = \Reviews\Architecture\Post_Types\Reviews::get_product_category_slug( $id );
-					$how_we_test = '<span class="hidden-xs">How We Tested <i class="fa fa-angle-right" aria-hidden="true"></i></span>';
-					if ( $slug === 'boards' ) {
-						$choosing_a_cat = '<span class="hidden-xs">Choosing a Board: </span>';
-					}
-          elseif ( $slug === 'drones' ) {
-						$choosing_a_cat = '<span class="hidden-xs">Choosing a Drone: </span>';
-					}
-          elseif ( $slug === 'printers' ) {
-						$choosing_a_cat = '<span class="hidden-xs">Choosing a 3D Printer: </span>';
-					}
-					echo $choosing_a_cat . $how_we_test;
-					?>
-					<span class="visible-xs-inline">How We Test</span>
+
+				<a <?php if ( \Reviews\Architecture\Post_Types\Reviews::is_review() ) { ?> class="active"  <?php } ?> href="<?php echo get_permalink( $id ); ?>">
+					Compare <?php echo $slug; ?> <i class="fa fa-angle-right" aria-hidden="true"></i>
 				</a>
 
-				<?php if ( \Reviews\Architecture\Post_Types\Reviews::is_scoring_enabled( $id ) ) { ?>
-					<a href="<?php echo \Reviews\Architecture\Post_Types\Reviews::get_scores_link( $id ); ?>"><span>About Scores <i class="fa fa-angle-right" aria-hidden="true"></i></span></a>
+				<a href="<?php echo \Reviews\Architecture\Post_Types\Reviews::get_how_we_test_link( $id ); ?>">
+					<?php
+					if ( $slug === 'boards' ) {
+						$choosing_a_cat = 'How to Choose <i class="fa fa-angle-right" aria-hidden="true"></i>';
+					}
+          elseif ( $slug === 'drones' ) {
+						$choosing_a_cat = 'How We Test <i class="fa fa-angle-right" aria-hidden="true"></i>';
+					}
+          elseif ( $slug === 'printers' ) {
+						$choosing_a_cat = 'How We Test <i class="fa fa-angle-right" aria-hidden="true"></i>';
+					}
+					echo $choosing_a_cat;
+					?>
+				</a>
+
+				<?php if ( \Reviews\Architecture\Post_Types\Reviews::is_scoring_enabled( $id ) ) {
+          if ( $slug === 'drones' ) {
+						$scoring = 'Drone Scoring';
+					}
+          elseif ( $slug === 'printers' ) {
+						$scoring = '3D Printer Scoring';
+					} ?>
+					<a href="<?php echo \Reviews\Architecture\Post_Types\Reviews::get_scores_link( $id ); ?>"><?php echo $scoring; ?> <i class="fa fa-angle-right" aria-hidden="true"></i></a>
 				<?php } ?>
 
 				<?php if ( \Reviews\Architecture\Post_Types\Reviews::is_review() ): ?>
 					<button id="show-filters-btn" type="button" class="show-filters-btn visible-xs-inline-block" aria-haspopup="true" aria-expanded="false">Filter</button>
 				<?php endif; ?>
 			</div>
-
-
-<!-- 				<li <?php if ( \Reviews\Architecture\Post_Types\Reviews::is_review() ) { ?> class="active"  <?php } ?> >
-				<a href="<?php echo get_permalink( $id ); ?>">Compare</a>
-			</li> -->
+			
 
 			<?php if ( \Reviews\Architecture\Post_Types\Reviews::is_review() ): ?>
 <!-- 				<div class="review-nav-btns visible-xs-block">
