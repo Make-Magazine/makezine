1c5042d163359ebdfdbe63c86363678ec045a756
David Beauchamp <dbeauchamp@makermedia.com>
2016-10-07 10:09:19 -0700

    Starting the layout for the 2016 gift guide

---
+++ wp-content/themes/makeblog/page-gift-guide-2016.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: Gift Guide 2016
+ *
+ * @package    makeblog
+ * @license    http://opensource.org/licenses/gpl-license.php  GNU Public License
+ * 
+ */
+get_header('version-2'); ?>
+
+<div id="gg2016">
+
+  <div class="gg2016-header container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-6 col-md-5">
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-7">
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+</div>
+
+<?php get_footer(); ?>
