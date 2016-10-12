ea7a4cf9b4acd118e67ca964895649f808656f01
David Beauchamp <dbeauchamp@makermedia.com>
2016-10-12 12:53:57 -0700

    Adding some demo products with some more layout

--- wp-content/themes/makeblog/page-gift-guide-2016.php
+++ wp-content/themes/makeblog/page-gift-guide-2016.php
@@ -12,16 +12,63 @@ get_header('version-2'); ?>
 
   <div class="gg2016-header container">
     <div class="row">
-      <div class="col-xs-12 col-sm-6 col-md-5">
+      <div class="col-xs-12 col-sm-6">
+        <h1>2016 Maker Gift Guide</h1>
+        <h2>Created by makers, for makers</h2>
+        <h3>Share your best tool, gadget, and project recommendations.</p>
       </div>
-      <div class="col-xs-12 col-sm-6 col-md-7">
+      <div class="col-xs-12 col-sm-6">
+        <img src="http://makezine.com/wp-content/uploads/2016/10/Screen-Shot-2016-10-06-at-5.25.37-PM.png" alt="Daily pick. Our recommended gift of the day." class="img-responsive" />
       </div>
     </div>
   </div>
 
+  <div class="gg2016-body">
+
+    <div class="gg2016-row-a row">
+      <div class="col-xs-12 col-sm-6">
+        <article class="row">
+          <a href="#" target="_blank">
+            <div class="col-xs-6 gg2016-row-img">
+              <img src="http://makezine.com/wp-content/uploads/2016/10/MakeKits-RaspPi-components_web_large.jpg" alt="2016 Maker Gift Guide Pick" class="img-responsive" />
+            </div>
+            <div class="col-xs-6 gg2016-row-desc">
+              <h4>Make: Getting Started with Raspberry Pi 3.0</h4>
+              <p>We made this special kit to contain the just the components you need to get started, including our book "Getting Started with Raspberry Pi - 3rd Edition".</p>
+              <p></p>
+              <span class="btn-cyan">Buy Now</a>
+            </div>
+          </a>
+        </article>
+        <article class="row">
+          <a href="#" target="_blank">
+            <div class="col-xs-6 gg2016-row-desc">
+              <h4></h4>
+              <p></p>
+              <p></p>
+              <span class="btn-cyan">Buy Now</a>
+            </div>
+            <div class="col-xs-6 gg2016-row-img">
+              <img src="http://makezine.com/wp-content/uploads/2016/10/EC2_1_large.jpg" alt="2016 Maker Gift Guide Pick" class="img-responsive" />
+            </div>
+          </a>
+        </article>
+      </div>
+      <div class="col-xs-12 col-sm-6 gg2016-maker-picks">
+
+      </div>
+    </div>
+
+    <div class="gg2016-row-b">
+
+    </div>
+
+    <div class="gg2016-row-c">
+
+    </div>
 
 
-
+  </div>
 
 </div>
 
