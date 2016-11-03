11da3511a14113cf18e494ea530030fe9c312103
David Beauchamp <dbeauchamp@makermedia.com>
2016-11-03 12:12:06 -0700

    Removing the AdRoll retargeting pixel now that we don't use it anymore

--- wp-content/themes/makeblog/js/footer-scripts/min/misc.js
+++ wp-content/themes/makeblog/js/footer-scripts/min/misc.js
@@ -131,24 +131,6 @@ jQuery(document).ready(function($) {
 });
 
 
-// AddRoll Retargeting Pixel
-adroll_adv_id = "KNRSJHIPMNCYTPL6CH6ZAM";
-adroll_pix_id = "65IC7MDQJVEX5LW2ZNA7YF";
-(function () {
-  var oldonload = window.onload;
-  window.onload = function(){
-    __adroll_loaded=true;
-    var scr = document.createElement("script");
-    var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
-    scr.setAttribute('async', 'true');
-    scr.type = "text/javascript";
-    scr.src = host + "/j/roundtrip.js";
-    ((document.getElementsByTagName('head') || [null])[0] ||
-    document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
-    if(oldonload){oldonload()}};
-}());
-
-
 // CrazyEgg
 // setTimeout(function(){var a=document.createElement("script");
 //   var b=document.getElementsByTagName("script")[0];
