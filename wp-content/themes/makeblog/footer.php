8c7c486877fffba50c0d909c1393883fc0e41595
David Beauchamp <dbeauchamp@makermedia.com>
2017-01-26 16:55:57 -0800

    Adding the Survey Gizmo Beacon to the footer for survey pop up

--- wp-content/themes/makeblog/footer.php
+++ wp-content/themes/makeblog/footer.php
@@ -220,6 +220,15 @@
 
 <?php wp_footer(); ?>
 
+<!-- Start Survey Gizmo Beacon -->
+<script type="text/javascript">
+(function(d,e,j,h,f,c,b){d.SurveyGizmoBeacon=f;d[f]=d[f]||function()
+{(d[f].q=d[f].q||[]).push(arguments)}
+;c=e.createElement(j),b=e.getElementsByTagName(j)[0];c.async=1;c.src=h;b.parentNode.insertBefore(c,b)})(window,document,'script','//d2bnxibecyz4h5.cloudfront.net/runtimejs/intercept/intercept.js','sg_beacon');
+sg_beacon('init','Mjg4MjQzLTJiMjdlNGU0NmM3ZDM3YWQ2YWJiOTEwNWRhNDM0ZGQ1NTFlZTdhN2Q3Y2E4Y2M0ZTlh');
+</script>
+<!-- End Survey Gizmo Beacon -->
+
 <!-- Checks the URL for which thank you modal to how -->
 <?php echo display_thank_you_modal_if_signed_up(); ?>
 
