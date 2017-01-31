702ea3b729800f92f6c92e5baae71fe8868e54ad
David Beauchamp <dbeauchamp@makermedia.com>
2017-01-30 18:01:45 -0800

    Adding a video option to the sidebar of the comparisons CPT

--- wp-content/themes/makeblog/reviews/content/sidebar/reviews.php
+++ wp-content/themes/makeblog/reviews/content/sidebar/reviews.php
@@ -6,8 +6,8 @@
 $how_scoring_works       = get_field( 'how_scoring_works' );
 $how_scoring_works_title = get_field( 'how_scoring_works_title' );
 $how_scoring_works_title = ! empty( $how_scoring_works_title ) ? $how_scoring_works_title : 'How Scoring Works';
-
 ?>
+
 <aside class="reviews-sidebar tc-sidebar mz-sidebar">
 
 	<div class="meta-block ad-1">
@@ -15,6 +15,42 @@ $how_scoring_works_title = ! empty( $how_scoring_works_title ) ? $how_scoring_wo
 	</div><!-- .meta-block.ad-1 -->
 
 	<?php
+	if( have_rows('youtube_videos') ): 
+		$yt_title = get_field('yt_sidebar_title');
+		$link_text = get_field('link_text');
+		$link = get_field('link'); ?>
+
+	    <div class="yt-video-sidebar">
+	    	<div class="yt-sidebar-title">
+	    		<i class="fa fa-play-circle-o" aria-hidden="true"></i><?php echo $yt_title; ?>
+	    	</div>
+	    	<div class="yt-sidebar-body">
+
+	  <?php
+	  while ( have_rows('youtube_videos') ) : the_row();
+
+	    $yt_id = get_sub_field('video_id');
+	    $description = get_sub_field('description2');
+	    ?>
+
+				<a class="fancytube fancybox.iframe" href="http://www.youtube.com/embed/<?php echo $yt_id; ?>?autoplay=1">
+					<div class="yt-sb-video">
+						<img class="img-responsive" src="http://img.youtube.com/vi/<?php echo $yt_id; ?>/mqdefault.jpg" alt="Product Review Videos" />
+						<img class="yt-play-btn" src="<?php echo get_stylesheet_directory_uri(); ?>/img/play-btn.png" alt="Youtube overlay play button" />
+					</div>
+					<p><?php echo $description; ?></p>
+				</a>
+
+	  <?php endwhile; ?>
+
+	    	</div>
+	    	<div class="yt-sidebar-footer">
+	    		<a href="<?php echo $link; ?>"><?php echo $link_text; ?></a>
+	    	</div>
+	    </div>
+
+	<?php endif;
+
 	$slug = \Reviews\Architecture\Post_Types\Reviews::get_product_category_slug( $id );
 	if ( $slug === 'boards' ) {
 		if ( is_active_sidebar( 'sidebar_comparison_boards' ) ) { ?>
