23db9ec4ea4c6918ae7444f5aaba73b197eeb22a
David Beauchamp <dbeauchamp@makermedia.com>
2017-12-12 21:11:27 +0800

    Now using PHP str_replace instead of javascript str_replace

--- wp-content/themes/makeblog/page-gift-guide-2017.php
+++ wp-content/themes/makeblog/page-gift-guide-2017.php
@@ -462,7 +462,7 @@ if( $cats ) { ?>
             if ($products) {
               foreach ($products as $product) { 
                 $productNamesFiltered = (str_replace(' ', '-', strtolower($product["product_name"])));
-                $productNamesFiltered = (str_replace(/[^a-zA-Z ]/g, '', $productNamesFiltered)); ?>
+                $productNamesFiltered = (str_replace(array('<', '>', '/', '!', ',', '?', '.'), '', $productNamesFiltered)); ?>
                 if(hash=='#<?php echo $productNamesFiltered; ?>') {
                   jQuery(window).bind("load", function() {
                     // Using jQuery's animate() method to add smooth page scroll
