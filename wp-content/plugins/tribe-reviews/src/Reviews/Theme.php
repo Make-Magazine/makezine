0fb6d31d37993c415670c2fa10ad23406f2247c3
Alicia Williams <alicia@makermedia.com>
2017-12-12 17:12:39 -0700

    force reviews js to pull in theme versioning

--- wp-content/plugins/tribe-reviews/src/Reviews/Theme.php
+++ wp-content/plugins/tribe-reviews/src/Reviews/Theme.php
@@ -19,10 +19,12 @@ class Theme {
 		}
 
 		// Enqueue our Product Review styles.
-		wp_enqueue_script( 'make-reviews', get_stylesheet_directory_uri() . '/reviews/js/src/index.js', array( 'jquery', 'bootstrap-js' ) );
+    $my_theme = wp_get_theme();
+    $my_version = $my_theme->get('Version');
+		wp_enqueue_script( 'make-reviews', get_stylesheet_directory_uri() . '/reviews/js/src/index.js', array( 'jquery', 'bootstrap-js' ),$my_version );
 
 		wp_localize_script( 'make-reviews', 'MakeReviews', array(
-			'ajax_url'    => set_url_scheme( admin_url( 'admin-ajax.php' ), 'https' ),
+			'ajax_url'    => set_url_scheme( admin_url( 'admin-ajax.php' ), 'http' ),
 			'ajax_action' => AJAX::FILTER_ACTION,
 			'post_id'     => get_the_ID()
 		) );
