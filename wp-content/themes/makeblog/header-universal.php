1aad32d31fc07e0ad954e245994c550848039b43
Alicia Williams <alicia@makermedia.com>
2018-04-05 10:51:11 -0600

    forgot part of the drop down

--- wp-content/themes/makeblog/header-universal.php
+++ wp-content/themes/makeblog/header-universal.php
@@ -247,11 +247,17 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
       <ul class="nav-level-1-auth">
         <li>
           <button id="qsLoginBtn">Login | Signup</button>
-          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-            <a class="dropdown-item" href="https://ww.makershare.com">MakerShare Profile</a>
-            <a class="dropdown-item" href="https://www.makerfaire.com/manage-entries">MakerFaire Portfolio</a>
-            <a class="dropdown-item" href="https://www.makeco.wpengine.com">My Membership Account</a>
-            <span class="dropdown-item" id="qsLogoutBtn">Log Out</span>
+          <div id="profile-view" class="dropdown show">
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <img class="avatar" style="width:38px" alt="avatar">
+            </a>
+
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+              <a class="dropdown-item" href="https://ww.makershare.com">MakerShare Profile</a>
+              <a class="dropdown-item" href="https://www.makerfaire.com/manage-entries">MakerFaire Portfolio</a>
+              <a class="dropdown-item" href="https://www.makeco.wpengine.com">My Membership Account</a>
+              <span class="dropdown-item" id="qsLogoutBtn">Log Out</span>
+            </div>
           </div>
         </li>
       </ul>
