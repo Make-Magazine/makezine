32c8caf1c9c31b520bd23c9610b06c0588474490
Rio Roth-Barrerio <rio@makermedia.com>
2018-06-07 14:55:42 -0700

    Remove My Account from dropdownmenu

--- wp-content/themes/makeblog/header-universal.php
+++ wp-content/themes/makeblog/header-universal.php
@@ -271,7 +271,6 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
             </a>
 
             <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-              <h3 class="expanding-underline">My Account</h3>
               <a class="dropdown-item" href="https://makershare.com/portfolio">Maker Share Profile</a>
               <a class="dropdown-item" href="https://makerfaire.com/manage-entries">Maker Faire Portal</a>
               <a class="dropdown-item" href="https://make.co/myaccount">My Membership Account</a>
