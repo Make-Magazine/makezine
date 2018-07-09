0fd873b37109ad48e9f82462557bfbbe2f38edd5
Alicia Williams <alicia@makermedia.com>
2018-07-09 15:16:29 -0600

    applying more streamlined auth0 login and logout process

--- wp-content/themes/makeblog/js/footer-scripts/auth0-variables.js
+++ wp-content/themes/makeblog/js/footer-scripts/auth0-variables.js
@@ -1,4 +1,3 @@
-var AUTH0_CLIENT_ID='0sR3MQz8ihaSnLstc1dABgENHS5PQR8d';
-var AUTH0_DOMAIN='makermedia.auth0.com';
+var AUTH0_CLIENT_ID    = '0sR3MQz8ihaSnLstc1dABgENHS5PQR8d';
+var AUTH0_DOMAIN       = 'makermedia.auth0.com';
 var AUTH0_CALLBACK_URL = templateUrl + "/authenticated/";
-var AUTH0_REDIRECT_URL = location.href;
