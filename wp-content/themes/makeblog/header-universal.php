af3b81510f5b56c17f766092f3f01d00ec49ea84
Rio Roth-Barrerio <rio@makermedia.com>
2018-09-12 09:46:13 -0700

    Make makezine profile dropdown match other sites better

--- wp-content/themes/makeblog/header-universal.php
+++ wp-content/themes/makeblog/header-universal.php
@@ -274,7 +274,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
               <a class="dropdown-item" href="https://makershare.com/portfolio">Makers Share Profile</a>
               <a class="dropdown-item" href="https://makerfaire.com/manage-entries">Maker Faire Portal</a>
               <a class="dropdown-item" href="https://make.co/myaccount">My Member Account</a>
-              <span class="dropdown-item" id="qsLogoutBtn">Log Out</span>
+              <a class="dropdown-item" id="qsLogoutBtn">Log Out</a>
             </div>
           </div>
         </li>
