7ea04ab9ad9ffc11d4f0079fc4de7a95ebc8df78
Travis Stone <travis@makermedia.com>
2018-10-31 15:51:05 -0700

    A few tweaks... - Add SumoMe - Update header text/images - add header changes to ACF

--- wp-content/themes/makeblog/page-gift-guide-general.php
+++ wp-content/themes/makeblog/page-gift-guide-general.php
@@ -18,6 +18,8 @@ if (have_posts()) {
       $page_subtitle_text = get_field('page_subtitle_text');
       $page_header_intro_text = get_field('page_header_intro_text');
       $page_header_hero_image = get_field('page_header_hero_image');
+      $page_header_sponsor_tag_text = get_field('page_header_sponsor_tag_text');
+      $page_header_sponsor_tag_image = get_field('page_header_sponsor_tag_image');
       $ad_frequency = get_field('ad_frequency');
    }
 } else {
@@ -27,19 +29,23 @@ if (have_posts()) {
 ?>
 
 <div class="gift-guide-container" data-ad-freq="<?php echo $ad_frequency; ?>">
+   <a class="social-buttons" data-sumome-share-id=66063407-7e20-418b-a8eb-03e9d38886be></a>
    <div class="hero-header">
       <div class="container">
          <div class="row">
             <div class="col-sm-12 title-image-block">
                <!-- TODO (ts): Ad goes here -->
                <div class="gg-header-ad"><div class="js-ad scroll-load" data-size="[[728,90],[970,90],[320,50]]" data-size-map="[[[1000,0],[[728,90],[970,90]]],[[800,0],[[728,90]]],[[0,0],[[320,50]]]]" data-pos="atf"></div></div>
-               <div class="page-title" style="background-image: url('<?php echo $page_title_image; ?>');background-size: contain;"><h2><?php echo $page_title_text; ?></h2></div>
+               <!-- <div class="page-title" style="background-image: url('<?php echo $page_title_image; ?>');background-size: contain;"><h2><?php echo $page_title_text; ?></h2></div> -->
                <h3 class="hero-subtitle"><?php echo $page_subtitle_text; ?></h3>
                <div class="header-intro-text"><?php echo $page_header_intro_text; ?></div>
-               <div class="hero-image hidden-md hidden-sm hidden-xs"><img src="<?php echo $page_header_hero_image; ?>" /></div>
             </div>
          </div>
       </div>
+   </div>
+   <div class="hero-image-container">
+      <div class="page-header-sponsor-tag hidden-lg hidden-xl"><img src="<?php echo $page_header_sponsor_tag_image;?>" alt="<?php echo $page_header_sponsor_tag_text;?>"></div>
+      <div class="hero-image hidden-md hidden-sm hidden-xs"><img src="<?php echo $page_header_hero_image; ?>" /></div>
    </div>
    <div id="gift-guide-temp"><h2 class="initial-loading-indicator" style="text-align: center;">Loading... <i class="fa fa-spinner"></i></h2></div>
    <div class="gg-footer-ad"><div class="js-ad scroll-load" data-size="[[728,90],[970,90],[320,50]]" data-size-map="[[[1000,0],[[728,90],[970,90]]],[[800,0],[[728,90]]],[[0,0],[[320,50]]]]" data-pos="btf"></div></div>
