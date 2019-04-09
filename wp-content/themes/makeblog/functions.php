1fb24ccce444e6e943459181f17ea0fe364f5c0e
Alicia Williams <alicia@makermedia.com>
2019-04-08 18:05:33 -0600

    wp 5.1.1 upgrade

--- wp-content/themes/makeblog/functions.php
+++ wp-content/themes/makeblog/functions.php
@@ -1,4 +1,10 @@
 <?php
+// FOR NOW, TURN OFF GUTENBURG
+// disable for posts
+add_filter('use_block_editor_for_post', '__return_false', 10);
+// disable for post types
+add_filter('use_block_editor_for_post_type', '__return_false', 10);
+
 // Easy Custom Fields Plugin
 require_once(WP_PLUGIN_DIR . '/easy-custom-fields/easy-custom-fields.php');
 /*
