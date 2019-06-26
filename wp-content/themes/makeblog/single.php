eff4c9571b761c60d7a4ad35dd19e9e31c542c97
Rio Roth-Barrerio <rio@makermedia.com>
2019-06-26 12:50:21 -0700

    remove google plus from easy social share and switch styling to flex box, remove old footer file and stylesheets

--- wp-content/themes/makeblog/single.php
+++ wp-content/themes/makeblog/single.php
@@ -180,7 +180,7 @@ if (user_can($current_user, 'administrator')) {
         <?php
         $url = str_replace(home_url(), 'https://makezine.com', get_permalink());
         $title = get_the_title();
-        echo do_shortcode('[easy-social-share buttons="facebook,twitter,google,reddit,pinterest,more" morebutton="2" morebutton_icon="dots" counters=1 counter_pos="bottom" total_counter_pos="hidden" style="button" nospace="yes" fullwidth="yes" template="metro-retina" url="' . $url . '" text="' . $title . '"]');
+        echo do_shortcode('[easy-social-share buttons="facebook,twitter,reddit,pinterest,love,more" morebutton="2" morebutton_icon="dots" counters=1 counter_pos="bottom" total_counter_pos="hidden" style="button" nospace="yes" fullwidth="yes" template="metro-retina" url="' . $url . '" text="' . $title . '"]');
         ?>
                                     <div class="article-body" itemprop="articleBody">
         <?php the_content(); ?>
