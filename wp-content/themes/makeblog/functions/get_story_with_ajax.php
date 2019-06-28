9c90f6e8f2df2ef55dfe837c8504c2a1b20ab410
Rio Roth-Barrerio <rio@makermedia.com>
2019-06-28 09:40:36 -0700

    remove google plus from ajax called stories

--- wp-content/themes/makeblog/functions/get_story_with_ajax.php
+++ wp-content/themes/makeblog/functions/get_story_with_ajax.php
@@ -73,7 +73,7 @@ function get_story_with_ajax() {
                     <?php
                     $url = str_replace(home_url(), 'https://makezine.com', get_permalink());
                     $title = get_the_title();
-                    echo do_shortcode('[easy-social-share buttons="facebook,twitter,google,reddit,pinterest,more" morebutton="2" morebutton_icon="dots" counters=1 counter_pos="bottom" total_counter_pos="hidden" style="button" nospace="yes" fullwidth="yes" template="metro-retina" url="'.$url.'" text="'.$title.'"]');
+                    echo do_shortcode('[easy-social-share buttons="facebook,twitter,reddit,pinterest,love,more" morebutton="2" morebutton_icon="dots" counters=1 counter_pos="bottom" total_counter_pos="hidden" style="button" nospace="yes" fullwidth="yes" template="metro-retina" url="'.$url.'" text="'.$title.'"]');
                     ?>
                     <div class="article-body" itemprop="articleBody">
                       <?php the_content(); ?>
