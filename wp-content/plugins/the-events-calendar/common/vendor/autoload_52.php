d2a685d5e1b7da80f2faf7f46b02b5e9c09afa04
Rio Roth-Barrerio <rio@makermedia.com>
2019-07-12 16:06:26 -0700

    update yoast, jetpack, event calendar plugins

--- wp-content/plugins/the-events-calendar/common/vendor/autoload_52.php
+++ wp-content/plugins/the-events-calendar/common/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit08392e81fbd882cbab933d73da4b0381::getLoader();
+return ComposerAutoloaderInitb532e80ffdcf48a76da03dac01cf7d7c::getLoader();
