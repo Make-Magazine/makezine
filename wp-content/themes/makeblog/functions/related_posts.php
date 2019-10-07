2b2ea55a38f548e6e7f680303964c26d38b55996
Rio Roth-Barrerio <rio@makermedia.com>
2019-10-07 11:43:08 -0700

    set default image for related articles

--- wp-content/themes/makeblog/functions/related_posts.php
+++ wp-content/themes/makeblog/functions/related_posts.php
@@ -6,6 +6,24 @@ function jetpackme_more_related_posts( $options ) {
 }
 add_filter( 'jetpack_relatedposts_filter_options', 'jetpackme_more_related_posts' );
 
+// set that default image
+function jetpack_default_image( $media, $post_id, $args ) {
+    if ( file_exists($media->src) ) {
+        return $media;
+    } else {
+        $permalink = get_permalink( $post_id );
+        $url = apply_filters( 'jetpack_photon_url', get_template_directory_uri() . '/img/default-related-article.png' );
+     
+        return array( array(
+            'type'  => 'image',
+            'from'  => 'custom_fallback',
+            'src'   => esc_url( $url ),
+            'href'  => $permalink,
+        ) );
+    }
+}
+add_filter( 'jetpack_images_get_images', 'jetpack_default_image', 10, 3 );
+
 // this would be to remove jetpack from being automatically added to all pages, so we could try to attach to the ajax pages
 /*function jetpackme_remove_rp() {
     if ( class_exists( 'Jetpack_RelatedPosts' ) ) {
