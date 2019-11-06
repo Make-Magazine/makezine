b371c345ec93462325a6e664e34f257487e0f988
Rio Roth-Barrerio <rio@makermedia.com>
2019-11-06 08:42:28 -0800

    social share for product review pages

--- wp-content/themes/makeblog/reviews/content/single/products.php
+++ wp-content/themes/makeblog/reviews/content/single/products.php
@@ -14,8 +14,12 @@ if (!empty($image)) {
 
             <meta itemprop="name" content="Make: Magazine">
 
-            <h2 class="product-title" itemprop="headline"><?php the_title(); ?></h2>
-
+            <h2 class="product-title" itemprop="headline"><?php the_title(); ?>!!!</h2>
+			   <?php // Social Share
+				  $url = str_replace(home_url(), 'https://makezine.com', get_permalink());
+				  $title = get_the_title();
+				  echo do_shortcode('[easy-social-share buttons="facebook,twitter,reddit,pinterest,love,more" morebutton="2" morebutton_icon="dots" counters=1 counter_pos="bottom" total_counter_pos="hidden" style="button" nospace="yes" fullwidth="yes" template="metro-retina" url="' . $url . '" text="' . $title . '"]');
+				?>	
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                     <div itemprop="description">
                         <?php the_content(); ?>
