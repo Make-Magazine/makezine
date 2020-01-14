e1a43729c3fd36567b2cb6d1eff253324c16f7b3
Rio Roth-Barrerio <rio@makermedia.com>
2020-01-14 15:07:51 -0800

    add the default pinterest tag

--- wp-content/themes/makeblog/header-universal.php
+++ wp-content/themes/makeblog/header-universal.php
@@ -1,4 +1,4 @@
-<?php
+d<?php
 /**
  * Makezine Universal Nav Template
  *
@@ -183,6 +183,24 @@ if( $detect->isTablet() ){
       ga('send', 'event', '404', document.location.href + document.location.search, document.referrer);
     </script>
   <?php endif; ?>
+	
+	<!-- Pinterest Tag -->
+	<script>
+	!function(e){if(!window.pintrk){window.pintrk = function () {
+	window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var
+	  n=window.pintrk;n.queue=[],n.version="3.0";var
+	  t=document.createElement("script");t.async=!0,t.src=e;var
+	  r=document.getElementsByTagName("script")[0];
+	  r.parentNode.insertBefore(t,r)}}("https://s.pinimg.com/ct/core.js");
+	pintrk('load', '2617466540835', {em: '<user_email_address>'});
+	pintrk('page');
+	</script>
+	<noscript>
+	<img height="1" width="1" style="display:none;" alt=""
+	  src="https://ct.pinterest.com/v3/?event=init&tid=2617466540835&pd[em]=<hashed_email_address>&noscript=1" />
+	</noscript>
+	<!-- end Pinterest Tag -->
+	
 </head>
 <body id="makeblog" <?php body_class(); ?>>
 
