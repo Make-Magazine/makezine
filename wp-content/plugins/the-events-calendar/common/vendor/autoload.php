f9fdaae8fc39c4e98f85d01453904139c807cfd5
Alicia Williams <alicia@make.co>
2020-06-17 14:49:33 -0600

    events plugin calendar update

--- wp-content/plugins/the-events-calendar/common/vendor/autoload.php
+++ wp-content/plugins/the-events-calendar/common/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit3a672380888faa1d26541e85d9fd9364::getLoader();
+return ComposerAutoloaderInit6107a73919450822ae4345b56c4c86ce::getLoader();
