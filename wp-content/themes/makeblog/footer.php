552a5ec1614c44722076e1c3e379ba32927ca3ba
Rio Roth-Barrerio <rio@makermedia.com>
2020-07-07 15:31:17 -0700

    active campaign pixel, gdpr compliant

--- wp-content/themes/makeblog/footer.php
+++ wp-content/themes/makeblog/footer.php
@@ -51,14 +51,30 @@
 </div>
 
 
-<!-- Start Survey Gizmo Beacon -->
-<script type="text/javascript">
-(function(d,e,j,h,f,c,b){d.SurveyGizmoBeacon=f;d[f]=d[f]||function()
-{(d[f].q=d[f].q||[]).push(arguments)}
-;c=e.createElement(j),b=e.getElementsByTagName(j)[0];c.async=1;c.src=h;b.parentNode.insertBefore(c,b)})(window,document,'script','//d2bnxibecyz4h5.cloudfront.net/runtimejs/intercept/intercept.js','sg_beacon');
-sg_beacon('init','Mjg4MjQzLTJiMjdlNGU0NmM3ZDM3YWQ2YWJiOTEwNWRhNDM0ZGQ1NTFlZTdhN2Q3Y2E4Y2M0ZTlh');
-</script>
-<!-- End Survey Gizmo Beacon -->
+
+
+<?php
+    // Tracking pixels users can turn off through the cookie law checkbox -- defaults to yes
+	if(!isset($_COOKIE['cookielawinfo-checkbox-non-necessary']) || $_COOKIE['cookielawinfo-checkbox-non-necessary'] == "yes" ) {
+?>
+		<!-- Start Active Campaign Pixel -->
+		<script type="text/javascript">
+			(function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]  function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q  []).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
+			vgo('setAccount', '1000801328');
+			vgo('setTrackByDefault', true);
+			vgo('process');
+		</script>
+		<!-- Start Active Campaign Pixel -->
+
+		<!-- Start Survey Gizmo Beacon -->
+		<script type="text/javascript">
+		(function(d,e,j,h,f,c,b){d.SurveyGizmoBeacon=f;d[f]=d[f]||function()
+		{(d[f].q=d[f].q||[]).push(arguments)}
+		;c=e.createElement(j),b=e.getElementsByTagName(j)[0];c.async=1;c.src=h;b.parentNode.insertBefore(c,b)})(window,document,'script','//d2bnxibecyz4h5.cloudfront.net/runtimejs/intercept/intercept.js','sg_beacon');
+		sg_beacon('init','Mjg4MjQzLTJiMjdlNGU0NmM3ZDM3YWQ2YWJiOTEwNWRhNDM0ZGQ1NTFlZTdhN2Q3Y2E4Y2M0ZTlh');
+		</script>
+		<!-- End Survey Gizmo Beacon -->
+<?php } ?>
 
 
 </body>
